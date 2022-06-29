189cf5e0a29601187975a049507821ea76e88a20
Rudy Rojas Gutierrez <rudy.rs1@gmail.com>
2022-06-29 17:47:25 -0400

    Staring exercise 01 - solution 01

---
+++ src/chapter01/P01_CountCharacters/P01_CountCharacters.md
@@ -0,0 +1,2 @@
+> ### CHAPTER 01
+> **_Exercise 01._ Couting duplicate characters:** Write a program that counts duplicate characters from a given string.
---
+++ src/chapter01/P01_CountCharacters/Solucion01/CountDuplicateCharacters.java
@@ -0,0 +1,9 @@
+package chapter01.P01_CountCharacters.Solucion01;
+
+public class CountDuplicateCharacters {
+    public static void main(String[] args) {
+
+
+    }
+
+}
